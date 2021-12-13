40edddc9baa30e87b72464e5f917dc0c0fa18f5e
Connor Minton <connor.c.minton@gmail.com>
2021-12-13 12:56:01 -0500

    implement escape pause

---
+++ Assets/Scripts/ObstacleCourseSceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCourseSceneManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePause();
+        }
+    }
+
+    private void Pause() {
+        if (!isPaused) {
+            isPaused = true;
+            Time.timeScale = 0;
+        }
+    }
+
+    private void Unpause() {
+        if (isPaused) {
+            isPaused = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    private void TogglePause() {
+        if (isPaused)
+            Unpause();
+        else Pause();
+    }
+}
